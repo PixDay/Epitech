2a8f2be78a48a6464c39d5121301602af90900ff
colombie <adrien.colombier@epitech.eu>
2020-03-29 19:57:22 -0400

    [ADD] - Task08, testing function using a main

---
+++ B1 - Unix & C Lab Seminar (Part I)/C Pool Day 03/tests/tests_my_put_nbr.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2020
+** tests_my_put_nbr.c
+** File description:
+** Adrien Colombier
+*/
+
+int my_put_nbr(int nb);
+
+int main()
+{
+    my_put_nbr(0);
+    my_putchar('\n');
+    my_put_nbr(1);
+    my_putchar('\n');
+    my_put_nbr(-1);
+    my_putchar('\n');
+    my_put_nbr(10);
+    my_putchar('\n');
+    my_put_nbr(-10);
+    my_putchar('\n');
+    my_put_nbr(128);
+    my_putchar('\n');
+    my_put_nbr(-189);
+    my_putchar('\n');
+    my_put_nbr(0x7FFFFFFF);
+    my_putchar('\n');
+    my_put_nbr(0x80000000);
+    my_putchar('\n');
+    return 0;
+}

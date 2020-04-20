897d16e1248e087377dda105fb826f05e0addb06
colombie <adrien.colombier@epitech.eu>
2020-04-20 14:41:46 +0200

    [ADD] - Task02, copying a part of a string

---
+++ B1 - Unix & C Lab Seminar (Part I)/C Pool Day 06/my_strncpy.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2020
+** my_strncpy.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+char *my_strncpy(char *dest, char const *src, int n)
+{
+    size_t index = 0;
+
+    while (src[index] != '\0' && index < n) {
+        dest[index] = src[index];
+        index++;
+    }
+    if (index > 0 && src[index] == '\0')
+        dest[index] = '\0';
+    return dest;
+}

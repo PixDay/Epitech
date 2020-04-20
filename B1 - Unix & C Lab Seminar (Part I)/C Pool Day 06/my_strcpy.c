a8544b41b7fa5535cd1086c203c86f59ee95fec7
colombie <adrien.colombier@epitech.eu>
2020-04-20 14:41:12 +0200

    [ADD] - Task01, copying a string

--- B1 - Unix & C Lab Seminar (Part I)/C Pool Day 06/my_strcpy.c
+++ B1 - Unix & C Lab Seminar (Part I)/C Pool Day 06/my_strcpy.c
@@ -9,8 +9,10 @@ char *my_strcpy(char *dest, char const *src)
 {
     size_t index = 0;
 
-    while (src[index++] != '\0')
+    while (src[index] != '\0') {
         dest[index] = src[index];
+        index++;
+    }
     dest[index] = '\0';
     return dest;
 }

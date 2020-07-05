9b9b135cf6513a56727ec987313e8e3de375825b
colombie <adrien.colombier@epitech.eu>
2020-07-05 17:22:33 +0200

    [ADD] - Task20, show string with non visible char in hexa way

---
+++ First Year/Semester I/B1 - Unix & C Lab Seminar (Part I)/C Pool Day 06/my_showstr.c
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2020
+** my_showstr.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void putHexa(int n)
+{
+    char const *str = "0123456789abcdef";
+    size_t left = n / 16;
+    size_t right = n % 16;
+
+    my_putchar('\\');
+    my_putchar(str[left]);
+    my_putchar(str[right]);
+}
+
+int my_showstr(char const *str)
+{
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        if ((int)(str[i]) >= ' ')
+            my_putchar(str[i]);
+        else
+            putHexa((int)(str[i]));    
+    }
+    return 0;
+}
+
+int main(void)
+{
+    my_showstr("Adrien \n Colombier");
+    return 0;
+}

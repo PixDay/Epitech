1478126a33a8be31815b0f0bb91079219f09ab8d
colombie <adrien.colombier@epitech.eu>
2020-07-07 14:28:10 +0200

    [ADD] - Task01, add strdup function allocating string memory

---
+++ First Year/Semester I/B1 - Unix & C Lab Seminar (Part I)/C Pool Day 08/my_strdup.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** my_strdup.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <stdlib.h>
+
+char *my_strdup(char const *src)
+{
+    size_t len = my_strlen(src);
+    char *dest = malloc((len * sizeof(char)) + 1);
+
+    if (dest == NULL)
+        return NULL;
+    for (size_t i = 0; src[i] != '\0'; i++);
+        dest[i] = src[i];
+    dest[len] = '\0';
+    return dest;
+}

ae07f53bf7f6f73df156fe34a732512bf1e6b921
colombie <adrien.colombier@epitech.eu>
2020-07-29 11:18:05 +0200

    [ADD] - Task09, sorting linked list

---
+++ First Year/Semester I/B1 - Unix & C Lab Seminar (Part I)/C Pool Day 11/my_sort_list.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2020
+** my_concat_list.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <stdlib.h>
+#include "include/mylist.h"
+
+void my_sort_list(linked_list_t **begin, int (*cmp)())
+{
+    int elements = my_list_size(begin[0]);
+    linked_list_t *tmp = NULL;
+
+    for (size_t i = 1; i < elements;) {
+        if (cmp(begin[i]->data, begin[i - 1]->data) < 0) {
+            tmp = begin[i];
+            begin[i] = begin[i - 1];
+            begin[i - 1] = tmp;
+            i -= 2;
+        }
+        i++;
+        i = (i < 1) ? 1 : i;
+    }
+    for (size_t i = 0; i < elements; i++)
+        begin[i]->next = begin[i + 1];
+    begin[elements - 1]->next = NULL;
+}

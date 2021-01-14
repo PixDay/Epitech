4f6070e2443c0acf87272a50db4ac3e7d4ec79e2
PixDay <adrien.colombier@epitech.eu>
2021-01-14 22:08:47 +0100

    [ADD] - Create bsq.c file containing major functions

---
+++ First Year/Semester I/Elementary Programming in C/BSQ/src/bsq.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2020
+** main.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include "bsq.h"
+
+int bsq(char const * file)
+{
+  bsq_t bsq;
+  struct stat fileInfo;
+  int error = 0;
+
+  error = stat(file, &fileInfo);
+  printf("error = %d\n", error);
+  printf("file size = %ld\n", fileInfo.st_size);
+  (void)bsq;
+  /*error = allocateBSQ(&bsq);
+  error = fillBSQ(&bsq);
+  if (error)
+    return FAILURE;
+  findBSQ(&bsq);
+  printBSQ(&bsq);*/
+  return EXIT_SUCCESS;
+}
+
+int allocateBSQ(bsq_t *bsq)
+{
+  (void)bsq;
+  return 0;
+}

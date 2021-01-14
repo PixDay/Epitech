625cbb72f89f0c1a3b9952b3408ecf35941822cd
PixDay <adrien.colombier@epitech.eu>
2021-01-14 22:04:36 +0100

    [ADD] - Create include/ folder and bsq header file

---
+++ First Year/Semester I/Elementary Programming in C/BSQ/include/bsq.h
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2021
+** bsq.h
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+/* STANDART INCLUDES */
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <stdio.h>
+
+/* DEFINES */
+#define FAILURE 84
+
+/* TYPES */
+typedef struct s_bsq {
+  int   fd;
+  char *fileContent;
+  char *charMap;
+  char *solvedMap;
+  int  *intMap;
+  int   nbLine;
+  int   lineSize;
+} bsq_t;
+
+/* PROTOTYPES */
+// MERLINE C LIBRARY
+int my_strlen(char const *str);
+
+// BSQ FUNCTIONS
+int bsq(char const * file);

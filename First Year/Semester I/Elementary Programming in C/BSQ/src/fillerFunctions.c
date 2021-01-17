6f7090ea76331cd8af3d865e418245e6bccf8ea9
PixDay <adrien.colombier@epitech.eu>
2021-01-17 11:35:32 +0100

    [ADD] - Create fillerFunctions C file that contain the different filler functions needed for BSQ

---
+++ First Year/Semester I/Elementary Programming in C/BSQ/src/fillerFunctions.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2020
+** fillerFunctions.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include "bsq.h"
+
+void fillFileContent(bsq_t *bsq)
+{
+  (void)bsq;
+}
+
+void fillCharMap(bsq_t *bsq)
+{
+  (void)bsq;
+}
+
+void fillSolvedMap(bsq_t *bsq)
+{
+  (void)bsq;
+}
+
+void fillIntMap(bsq_t *bsq)
+{
+  (void)bsq;
+}

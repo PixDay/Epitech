028eb35227cc71ff7983543632ba8d56b2159750
PixDay <adrien.colombier@epitech.eu>
2021-01-17 22:21:25 +0100

    [MODIFIED] - Implementing fillerFunctions will need UT

--- First Year/Semester I/Elementary Programming in C/BSQ/src/fillerFunctions.c
+++ First Year/Semester I/Elementary Programming in C/BSQ/src/fillerFunctions.c
@@ -15,15 +15,48 @@ void fillFileContent(bsq_t *bsq)
 
 void fillCharMap(bsq_t *bsq)
 {
-  
+  uint32_t startingMap = 0;
+  uint32_t lineSize = 0;
+
+  for (uint32_t i = 0; i < bsq->fileSize; i++)
+    bsq->charMap[i] = '\0';
+  while (bsq->fileContent[startingMap++] != '\n');
+  for (uint32_t i = 0; bsq->fileContent[startingMap + i] != '\0'; i++) {
+    bsq->charMap[i] = bsq->fileContent[startingMap + i];
+    lineSize++;
+    if (bsq->charMap[i] == '\n') {
+      bsq->nbLine++;
+      if (bsq->lineSize == 0)
+        bsq->lineSize = lineSize;
+      if (lineSize != bsq->lineSize)
+        bsq->error = FAILURE;
+      lineSize = 0;
+    }
+  }
 }
 
 void fillSolvedMap(bsq_t *bsq)
 {
-  (void)bsq;
+  uint32_t index = 0;
+
+  while (bsq->charMap[index] != '\0') {
+    bsq->solvedMap[index] = bsq->charMap[index];
+    index++;
+  }
+  bsq->solvedMap[index] = '\0';
 }
 
 void fillIntMap(bsq_t *bsq)
 {
-  (void)bsq;
+  uint32_t index = 0;
+
+  while (bsq->charMap[index] != '\0') {
+    bsq->intMap[index] = 1;
+    if (bsq->charMap[index] == 'o')
+      bsq->intMap[index] = 0;
+    if (bsq->charMap[index] == '\n')
+      bsq->intMap[index] = EOL;
+    index++;
+  }
+  bsq->intMap[index] = EOM;
 }

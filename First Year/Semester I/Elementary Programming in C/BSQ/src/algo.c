037bcf4fe4848934c3212705871ada5b742572c9
PixDay <adrien.colombier@epitech.eu>
2021-01-18 19:34:28 +0100

    [ADD] - Creating algo C file

---
+++ First Year/Semester I/Elementary Programming in C/BSQ/src/algo.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** algo.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include "bsq.h"
+
+void algoUpdateIntMap(bsq_t *bsq)
+{
+  (void)bsq;
+}
+
+void algoWriteSolution(bsq_t *bsq)
+{
+  (void)bsq;
+}

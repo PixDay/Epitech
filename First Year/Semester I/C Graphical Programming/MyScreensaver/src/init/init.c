fd244bbebccc78ed87254d4554ce933f0487038c
PixDay <adrien.colombier@epitech.eu>
2021-02-01 02:00:19 +0100

    [ADD] - Create init folder and init.c for global initilisation

---
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/init/init.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** init.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <screenSaver.h>
+
+size_t globalInit(screenSaver_t *screenSaver)
+{
+  return EXIT_SUCCESS;
+}

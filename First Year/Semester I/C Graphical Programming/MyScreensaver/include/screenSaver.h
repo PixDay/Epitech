5f6b39af498c2a5ec800f08bf15962f57e163f16
PixDay <adrien.colombier@epitech.eu>
2021-02-01 01:53:45 +0100

    [MODIFIED] - screenSaver.h header file updated with csfml includes and first types

--- First Year/Semester I/C Graphical Programming/MyScreensaver/include/screenSaver.h
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/include/screenSaver.h
@@ -9,8 +9,18 @@
 
 /* STANDART INCLUDES */
 #include <stdlib.h>
+#include <SFML/Graphics.h>
+#include <SFML/Audio.h>
 
 /* DEFINES */
 #define FAILURE 84
 
 /* TYPES */
+typedef struct s_screenSaver {
+  sfRenderWindow *window;
+  sfTexture     **textures;
+  sfSprite      **sprites;
+} screenSaver_t;
+
+/* PROTOTYPES */
+size_t screenSaver(int argc, char **argv);

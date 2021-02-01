b9478e8aa0068e6f61c98640c752701172c694db
PixDay <adrien.colombier@epitech.eu>
2021-02-01 01:45:05 +0100

    [ADD] - Create include folder with the default screenSaver header file

---
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/include/screenSaver.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** screenSaver.h
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#pragma once
+
+/* STANDART INCLUDES */
+#include <stdlib.h>
+
+/* DEFINES */
+#define FAILURE 84
+
+/* TYPES */

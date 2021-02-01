35817c6c2871d8c08f44f41284ab756b6c3e0bbc
PixDay <adrien.colombier@epitech.eu>
2021-02-01 01:54:07 +0100

    [ADD] - Create screenSaver.c file

---
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/screenSaver.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** screenSaver.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <screenSaver.h>
+
+size_t screenSaver(int argc, char **argv)
+{
+  (void)argc;
+  (void)argv;
+  return EXIT_SUCCESS;
+}

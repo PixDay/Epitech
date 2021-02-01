afe3987ea5f53afeff60db6427fc52241f5d49e1
PixDay <adrien.colombier@epitech.eu>
2021-02-01 01:42:09 +0100

    [ADD] - Create src folder and empty main function

---
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** main.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  (void)argc;
+  (void)argv;
+  return EXIT_SUCCESS;
+}

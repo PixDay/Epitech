a2069b9795c14773a42af155e84f9cb97f4f0c76
PixDay <adrien.colombier@epitech.eu>
2021-02-11 23:16:09 +0100

    [MODIFIED] - Splitting init

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/init/init.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/init/init.c
@@ -9,14 +9,16 @@
 
 size_t globalInit(engine_t *engine)
 {
-  engine->app.mode = (sfVideoMode){1280, 960, 32};
-  engine->app.window = sfRenderWindow_create(engine->app.mode, "RUN", sfResize | sfClose, NULL);
-  if (engine->app.window == NULL)
+  if (appInit(engine))
     return FAILURE;
   return EXIT_SUCCESS;
 }
 
 size_t appInit(engine_t *engine)
 {
-  
+  engine->app.mode = (sfVideoMode){1920, 1080, 32};
+  engine->app.window = sfRenderWindow_create(engine->app.mode, "SCREENSAVER", sfFullscreen | sfClose, NULL);
+  if (engine->app.window == NULL)
+    return FAILURE;
+  return EXIT_SUCCESS;
 }

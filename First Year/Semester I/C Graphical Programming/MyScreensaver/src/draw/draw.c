ca5e2fda4700059cd0ad92d4ca5a571d5d3b430f
PixDay <adrien.colombier@epitech.eu>
2021-02-11 23:32:19 +0100

    [ADD] - Create draw folder and default draw.c file

---
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/draw.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** draw.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <screenSaver.h>
+
+void drawArrow(engine_t *engine)
+{
+  (void)engine;
+}

93156f4dc8f71d836e2b9a1e2423d4f8719f1451
PixDay <adrien.colombier@epitech.eu>
2021-02-11 20:54:16 +0100

    [MODIFIED] - Add space between variable and operators

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/init/init.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/init/init.c
@@ -10,7 +10,7 @@
 size_t globalInit(engine_t *engine)
 {
   engine->app.mode = (sfVideoMode){1280, 960, 32};
-  engine->app.window = sfRenderWindow_create(engine->app.mode, "RUN", sfResize| sfClose, NULL);
+  engine->app.window = sfRenderWindow_create(engine->app.mode, "RUN", sfResize | sfClose, NULL);
   if (engine->app.window == NULL)
     return FAILURE;
   return EXIT_SUCCESS;

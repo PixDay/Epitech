6519fe601a2c3bc138a9cacd3c9f092083e74ccd
PixDay <adrien.colombier@epitech.eu>
2021-02-12 01:51:32 +0100

    [MODIFIED] - Create the hover like function for arrows

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/draw.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/draw.c
@@ -11,11 +11,15 @@ void drawArrow(engine_t *engine)
 {
   sfVector2f reverseScale = {-1.0f, -1.0f};
   sfVector2f positionOrigin = {256.0f, 1080.0f};
-  
+  sfFloatRect leftArrow = {0.0f, 0.0f, 256.0f, 1080.f};
+  sfFloatRect rightArrow = {1920.0f - 256.0f, 0.0f, 256.0f, 1080.0f};
+
   sfSprite_scale(engine->nextSprite, reverseScale);
   sfSprite_setPosition(engine->nextSprite, positionOrigin);
-  sfRenderWindow_drawSprite(engine->app.window, engine->nextSprite, NULL);
+  if (sfFloatRect_contains(&leftArrow, engine->cursor.x, engine->cursor.y))
+    sfRenderWindow_drawSprite(engine->app.window, engine->nextSprite, NULL);
   sfSprite_setPosition(engine->nextSprite, (sfVector2f){1920.0f - 256.0f, 0.0f});
   sfSprite_scale(engine->nextSprite, reverseScale);
-  sfRenderWindow_drawSprite(engine->app.window, engine->nextSprite, NULL);
+  if (sfFloatRect_contains(&rightArrow, engine->cursor.x, engine->cursor.y))
+    sfRenderWindow_drawSprite(engine->app.window, engine->nextSprite, NULL);
 }

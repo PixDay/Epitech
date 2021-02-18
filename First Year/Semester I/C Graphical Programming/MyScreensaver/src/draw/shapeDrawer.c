e9a505f64099e110d265cb8312b0907bad4edd28
PixDay <adrien.colombier@epitech.eu>
2021-02-18 21:17:32 +0100

    [ADD] - Create shapeDrawer file and create drawRect teplate function

---
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/shapeDrawer.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** shapeDrawer.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <screenSaver.h>
+
+void drawRect(sfVector2f position, sfVector2f size, uint32_t color, engit_t *engine)
+{
+  printf("DRAW RECT");
+}

aa24ac68666154e210f64346256cd81546c7180e
PixDay <adrien.colombier@epitech.eu>
2021-02-18 21:22:31 +0100

    [ADD] - Create drawCircle template function

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/shapeDrawer.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/shapeDrawer.c
@@ -15,3 +15,12 @@ void drawRect(sfVector2f position, sfVector2f size, uint32_t color, engine_t *en
   (void)engine;
   printf("DRAW RECT");
 }
+
+void drawCircle(sfVector2f position, uint32_t size, uint32_t color, engine_t *engine)
+{
+  (void)position;
+  (void)size;
+  (void)color;
+  (void)engine;
+  printf("DRAW CIRCLE");
+}

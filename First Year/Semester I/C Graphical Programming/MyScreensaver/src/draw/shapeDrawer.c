fe64bddb8c72e34f20ead691800150336b1211de
PixDay <adrien.colombier@epitech.eu>
2021-02-24 15:55:04 +0100

    [MODIFIED] - Implementing drawRect function

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/shapeDrawer.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/draw/shapeDrawer.c
@@ -9,10 +9,15 @@
 
 void drawRect(sfVector2f position, sfVector2f size, uint32_t color, engine_t *engine)
 {
-  (void)position;
-  (void)size;
-  (void)color;
-  (void)engine;
+  sfVector2i coord = {0, 0};
+
+  for (uint32_t y = 0; y < size.y; y++) {
+    for (uint32_t x = 0; x < size.x; x++) {
+      coord.x = position.x + x;
+      coord.y = position.y + y * HD_LINE_SIZE;
+      engine->screenSavers[engine->currentScreen].pixelBuffer[coord.y + coord.x] = color;
+    }
+  }
 }
 
 void drawCircle(sfVector2f position, uint32_t size, uint32_t color, engine_t *engine)

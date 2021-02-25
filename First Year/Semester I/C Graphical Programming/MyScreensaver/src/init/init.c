573e105d2e507f924809e50cbcd0c4086b5f1031
PixDay <adrien.colombier@epitech.eu>
2021-02-26 00:21:06 +0100

    [MODIFIED] - Init slidingRect array

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/init/init.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/init/init.c
@@ -28,16 +28,20 @@ size_t appInit(engine_t *engine)
 
 size_t screenSaverInit(engine_t *engine)
 {
+  size_t rects = 20;
+
   engine->currentScreen = 0;
   engine->nbOfScreen = 3;
-  engine->screenSavers = malloc(engine->nbOfScreen * sizeof(screenSaver_t));
-  if (engine->screenSavers == NULL)
+  engine->screenSavers.rects = malloc(rects * sizeof(slidingRect_t));
+  engine->screenSavers.pixelBuffer = malloc((HD) * RGBA * sizeof(sfUint8));
+  engine->screenSavers.pixelSprite = sfSprite_create();
+  if (engine->screenSavers.pixelBuffer == NULL || engine->screenSavers.rects == NULL)
     return FAILURE;
-  for (size_t scene = 0; scene < engine->nbOfScreen; scene++) {
-    engine->screenSavers[scene].pixelBuffer = malloc((HD) * RGBA * sizeof(sfUint8));
-    engine->screenSavers[scene].pixelSprite = sfSprite_create();
-    if (engine->screenSavers[scene].pixelBuffer == NULL || engine->screenSavers[scene].pixelSprite == NULL)
-      return FAILURE;
+  for (size_t i = 0; i < rects; i++) {
+    engine->screenSavers.rects->status = 'I';
+    engine->screenSavers.rects->direction = rand() % 2 ? 'R' : 'L';
+    engine->screenSavers.rects->position = (sfVector2f){rand() % 300 + 960, rand() % 900};
+    engine->screenSavers.rects->size = (sfVector2f){rand() % 250 + 100, rand() % 50 + 50};
   }
   return EXIT_SUCCESS;
 }

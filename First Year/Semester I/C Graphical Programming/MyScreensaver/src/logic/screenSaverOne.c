f07525956a869f2f56963cbab88f24c3e26137be
PixDay <adrien.colombier@epitech.eu>
2021-02-27 11:26:10 +0100

    [MODIFIED] - Create logic folder and first screenSaver

---
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/logic/screenSaverOne.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** screenSaverOne.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include <screenSaver.h>
+
+void screenSaverOneLogic(engine_t *engine)
+{
+  (void)engine;
+}

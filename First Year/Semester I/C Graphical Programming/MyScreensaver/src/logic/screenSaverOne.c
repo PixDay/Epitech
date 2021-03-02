ce5ba2af930539d21ed7b6c315104afc0f6e809e
PixDay <adrien.colombier@epitech.eu>
2021-03-02 15:15:58 +0100

    [MODIFIED] - Implemented the sliding effect to the right

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/logic/screenSaverOne.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/logic/screenSaverOne.c
@@ -10,14 +10,20 @@
 void screenSaverOneLogic(engine_t *engine)
 {
   for (size_t i = 0; i < RECTS; i++) {
-    if (engine->screenSavers.rects[i].size.x == engine->screenSavers.rects[i].originalSize.x)
-      engine->screenSavers.rects[i].size.x = 0.0f;
-    if (engine->screenSavers.rects[i].direction == 'R') {
-      // RIGHT
-
-    } else {
-      // LEFT
-
+    if (engine->screenSavers.rects[i].size.x <= 0.0f)
+      engine->screenSavers.rects[i].status = 'R';
+    if (engine->screenSavers.rects[i].status == 'R') {
+      engine->screenSavers.rects[i].status = 'I';
+      engine->screenSavers.rects[i].size.x = 1.0f;
+      engine->screenSavers.rects[i].position.x = engine->screenSavers.rects[i].originalPos.x;
+    }
+    if (engine->screenSavers.rects[i].size.x >= engine->screenSavers.rects[i].originalSize.x && engine->screenSavers.rects[i].status == 'I')
+      engine->screenSavers.rects[i].status = 'S';
+    if (engine->screenSavers.rects[i].direction == 'R' && engine->screenSavers.rects[i].status == 'I')
+      engine->screenSavers.rects[i].size.x++;
+    if (engine->screenSavers.rects[i].direction == 'R' && engine->screenSavers.rects[i].status == 'S') {
+      engine->screenSavers.rects[i].position.x++;
+      engine->screenSavers.rects[i].size.x--;
     }
   }
 }

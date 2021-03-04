ac5020ca3dc2d36c126908d8597c4af81ae3a886
PixDay <adrien.colombier@epitech.eu>
2021-03-04 18:28:52 +0100

    [MODIFIED] - Fix range and adapt code

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/logic/sceneFunctions.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/logic/sceneFunctions.c
@@ -11,11 +11,10 @@ void changeScene(engine_t *engine)
 {
   int state = 0;
 
-  if (engine->cursor.x < 50.0f)
+  if (engine->cursor.x < 256.0f)
     state = -1;
-  else if (engine->cursor.x > 1870.0f)
+  else if (engine->cursor.x > 1664.0f)
     state = 1;
-  engine->currentScreen += state;
-  engine->currentScreen = (engine->currentScreen == -1) ? engine->nbOfScreen - 1 : engine->currentScreen;
-  engine->currentScreen = (engine->currentScreen == engine->nbOfScreen) ? 0 : engine->currentScreen;
+  state = engine->currentScreen + state;
+  engine->currentScreen = (state == -1) ? engine->nbOfScreen - 1 : (state == (int)engine->nbOfScreen) ? 0 : (size_t)state; 
 }

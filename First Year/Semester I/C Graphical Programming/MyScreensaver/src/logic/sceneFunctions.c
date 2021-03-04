fe4857b91f1f47b9c77de5561a58ebfc6e37f531
PixDay <adrien.colombier@epitech.eu>
2021-03-04 18:09:13 +0100

    [MODIFIED] - Implement sceneFunctions

--- First Year/Semester I/C Graphical Programming/MyScreensaver/src/logic/sceneFunctions.c
+++ First Year/Semester I/C Graphical Programming/MyScreensaver/src/logic/sceneFunctions.c
@@ -7,3 +7,15 @@
 
 #include "screenSaver.h"
 
+void changeScene(engine_t *engine)
+{
+  int state = 0;
+
+  if (engine->cursor.x < 50.0f)
+    state = -1;
+  else if (engine->cursor.x > 1870.0f)
+    state = 1;
+  engine->currentScreen += state;
+  engine->currentScreen = (engine->currentScreen == -1) ? engine->nbOfScreen - 1 : engine->currentScreen;
+  engine->currentScreen = (engine->currentScreen == engine->nbOfScreen) ? 0 : engine->currentScreen;
+}

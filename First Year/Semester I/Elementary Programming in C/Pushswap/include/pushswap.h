ae7de6f5b4c05b6f1da33e6d8834f661aac9fc0d
PixDay <adrien.colombier@epitech.eu>
2021-10-11 12:37:10 +0200

    [EDIT] - Update Pushswap function prototypes

--- First Year/Semester I/Elementary Programming in C/Pushswap/include/pushswap.h
+++ First Year/Semester I/Elementary Programming in C/Pushswap/include/pushswap.h
@@ -22,8 +22,9 @@
 // MERLINE C LIBRARY
 int my_strlen(char const *str);
 int my_getnbr(char const *str);
+int is_number(char const *str);
 
 // PUSHSWAP FUNCTIONS
 uint8_t check_args(int argc, char **argv);
-uint8_t alloc_and_fille_number_in_array(argc, argv, array_a);
+uint8_t alloc_and_fille_number_in_array(int argc, char **argv, int32_t array_a);
 void    sort(int32_t *array_a, uint32_t array_size);

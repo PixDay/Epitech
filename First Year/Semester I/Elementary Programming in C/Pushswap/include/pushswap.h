4891d3a0f6aea5d5b86f2583d02a6b0eb2ca755e
PixDay <adrien.colombier@epitech.eu>
2021-10-11 11:34:43 +0200

    [ADD] - Create include folder and pushswap header file

---
+++ First Year/Semester I/Elementary Programming in C/Pushswap/include/pushswap.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2021
+** pushswap.h
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#pragma once
+
+/* STANDART INCLUDES */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/* DEFINES */
+#define FAILURE 84
+#define SUCCESS 0
+#define BLOCK   1024
+
+/* TYPES */
+
+// MERLINE C LIBRARY
+int my_strlen(char const *str);
+int my_getnbr(char const *str);
+
+// PUSHSWAP FUNCTIONS

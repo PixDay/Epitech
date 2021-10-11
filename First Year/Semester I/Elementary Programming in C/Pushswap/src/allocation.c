08eddd1459030695add2b39755e93cdc99037412
PixDay <adrien.colombier@epitech.eu>
2021-10-11 12:48:13 +0200

    [ADD] - Allocation.c file have all function implemented

--- First Year/Semester I/Elementary Programming in C/Pushswap/src/allocation.c
+++ First Year/Semester I/Elementary Programming in C/Pushswap/src/allocation.c
@@ -6,3 +6,15 @@
 */
 
 #include "pushswap.h"
+
+uint8_t alloc_and_fill_number_in_array(int argc, char **argv, int32_t *array_a)
+{
+    array_a = malloc((argc - 1) * sizeof(int32_t));
+    if (array_a == NULL)
+        return FAILURE;
+    for (int32_t i = 1; i < argc; i++)
+    {
+        array_a[i] = my_getnbr(argv[i]);
+    }
+    return SUCCESS;
+}

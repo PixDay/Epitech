75389a0d3842f0b69a2a26076b6e8ae7abe72f89
PixDay <adrien.colombier@epitech.eu>
2021-10-11 11:53:30 +0200

    [EDIT] - Implement main function

--- First Year/Semester I/Elementary Programming in C/Pushswap/src/main.c
+++ First Year/Semester I/Elementary Programming in C/Pushswap/src/main.c
@@ -9,7 +9,15 @@
 
 int main(int argc, char **argv)
 {
+  int32_t *array_a = NULL;
+  uint32_t array_size = argc - 1;
+
   if (argc < 2)
     return FAILURE;
-  return 0;//bsq(argv[1]);
+  if (check_args(argc, argv))
+    return FAILURE;
+  if (alloc_and_fille_number_in_array(argc, argv, array_a))
+    return FAILURE;
+  sort(array_a, array_size);
+  return SUCCESS;
 }

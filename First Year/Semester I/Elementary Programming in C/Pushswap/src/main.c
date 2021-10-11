1b7166559190125e8a315b7bfb3071e3e684e376
PixDay <adrien.colombier@epitech.eu>
2021-10-11 12:48:58 +0200

    [UPDATE] - Updating main function with proper call function and good parameters

--- First Year/Semester I/Elementary Programming in C/Pushswap/src/main.c
+++ First Year/Semester I/Elementary Programming in C/Pushswap/src/main.c
@@ -12,12 +12,22 @@ int main(int argc, char **argv)
   int32_t *array_a = NULL;
   uint32_t array_size = argc - 1;
 
-  if (argc < 2)
-    return FAILURE;
   if (check_args(argc, argv))
     return FAILURE;
-  if (alloc_and_fille_number_in_array(argc, argv, array_a))
+  if (alloc_and_fill_number_in_array(argc, argv, array_a))
     return FAILURE;
   sort(array_a, array_size);
+  return SUCCESS;
+}
+
+uint8_t check_args(int argc, char **argv)
+{
+  if (argc < 2)
+    return FAILURE;
+  for (uint32_t i = 1; i < (uint32_t)argc; i++)
+  {
+    if (is_number(argv[i]) != 0)
+      return FAILURE;
+  }
   return SUCCESS;
 }

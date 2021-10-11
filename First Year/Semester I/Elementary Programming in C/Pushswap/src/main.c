229405e12b0bb99b83b9262458a33b0a3921606f
PixDay <adrien.colombier@epitech.eu>
2021-10-11 11:34:55 +0200

    [ADD] - Create src folder and main.c file

---
+++ First Year/Semester I/Elementary Programming in C/Pushswap/src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** main.c
+** File description:
+** adrien.colombier@epitech.eu
+*/
+
+#include "pushswap.h"
+
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+    return FAILURE;
+  return 0;//bsq(argv[1]);
+}
